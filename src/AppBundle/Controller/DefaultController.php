0afe76dd3054e372c17ac27c2288aac6bd3c204f
Yasser Mussa <yasser.mussa@gmail.com>
2017-01-04 08:52:12 -0300

    routing and json method

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,6 +5,10 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\GetSetMethodNormalizer;
+use Symfony\Component\Serializer\Serializer;
 
 class DefaultController extends Controller
 {
@@ -26,7 +30,21 @@ class DefaultController extends Controller
     {
         $em = $this->getDoctrine()->getManager();
         $users = $em->getRepository('BackendBundle:User')->findAll();
-        var_dump($users);
-        die();
+        return $this->json($users);
+    }
+
+    public function json($data)
+    {
+        $normalizers = array(new GetSetMethodNormalizer());
+        $encoders = array("json" => new JsonEncoder());
+
+        $serializer = new Serializer($normalizers, $encoders);
+        $json = $serializer->serialize($data, 'json');
+
+        $response = new Response();
+        $response->setContent($json);
+        $response->headers->set("Content-Type", "application/json");
+
+        return $response;
     }
 }
